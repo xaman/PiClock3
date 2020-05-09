443f38aa7a09d64ac46fd969097c0c736aca74d6
Martin Chamarro <martin.chamarro@asos.com>
2020-05-09 14:15:49 +0100

    Adds a class for the HTTP requests

---
+++ data/network/request.py
@@ -0,0 +1,84 @@
+# -*- coding: UTF-8 -*-
+from io import BytesIO
+import pycurl
+import logging
+
+
+class Request(object):
+    ENCODING = 'utf-8'
+    COOKIES_FILE = '/var/tmp/cookies.txt'
+    VERBOSE = False
+    TIMEOUT_IN_SECONDS = 30
+
+    def __init__(self, url):
+        self.url = url
+        self.is_post = True
+        self.headers = []
+        self.data = ''
+        self.curl = pycurl.Curl()
+        self.buffer = BytesIO()
+        self.logger = logging.getLogger('data')
+
+    def add_header(self, header):
+        self.headers.append(header)
+        return self
+
+    def get_headers(self):
+        return self.headers
+
+    def set_headers(self, headers):
+        self.headers = headers
+        return self
+
+    def add_cookie(self, cookie):
+        self.curl.setopt(pycurl.COOKIE, cookie)
+        return self
+
+    def set_data(self, data):
+        self.data = data
+        return self
+
+    def referer(self, referer):
+        self.headers.append('Referer: ' + referer)
+        return self
+
+    def post(self):
+        self._perform(True)
+        return self
+
+    def get(self):
+        self._perform(False)
+        return self
+
+    def response(self):
+        return self.buffer.getvalue().decode(self.ENCODING)
+
+    def close(self):
+        self.buffer.close()
+
+    def accept_gzip(self):
+        self.curl.setopt(pycurl.ENCODING, 'gzip')
+
+    def _perform(self, is_post):
+        self.curl.setopt(pycurl.VERBOSE, self.VERBOSE)
+        self.logger.debug("Request: " + self.url)
+        self.curl.setopt(pycurl.URL, self.url)
+        self.curl.setopt(pycurl.SSL_VERIFYHOST, 0)
+        self.curl.setopt(pycurl.SSL_VERIFYPEER, False)
+        self.curl.setopt(pycurl.CONNECTTIMEOUT, self.TIMEOUT_IN_SECONDS)
+        self.curl.setopt(pycurl.TIMEOUT, self.TIMEOUT_IN_SECONDS)
+        self.curl.setopt(pycurl.WRITEDATA, self.buffer)
+        self.curl.setopt(pycurl.POST, is_post)
+        self.curl.setopt(pycurl.COOKIEFILE, self.COOKIES_FILE)
+        self.curl.setopt(pycurl.COOKIEJAR, self.COOKIES_FILE)
+        self.curl.setopt(pycurl.FOLLOWLOCATION, False)
+        if len(self.data) > 0:
+            self.logger.debug("Body: " + self.data)
+            self.curl.setopt(pycurl.POSTFIELDS, self.data)
+            self.curl.setopt(pycurl.POSTFIELDSIZE, len(self.data))
+        self.logger.debug("Headers: " + str(self.headers))
+        self.curl.setopt(pycurl.HTTPHEADER, self.headers)
+        try:
+            self.curl.perform()
+        except pycurl.error as e:
+            self.logger.error(str(e))
